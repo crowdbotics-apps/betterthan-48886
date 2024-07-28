9a7909847ec2d549bcce88ff053c5ea5b5bb876c
Crowdbotics <team@crowdbotics.com>
2024-07-28 01:10:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "betterthan-48886",
+  url: "https://betterthan-48886.botics.co",
+  api: "https://betterthan-48886.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
